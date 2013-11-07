1f87ec3d160495abbb71ea1319151382b1d32ae9
Lisa P <passinglisa@gmail.com>
2013-11-07 18:46:41 +0100

    add project files

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'pathname'
+
+get '/' do
+  '<h1>Chat with me</h1>' +
+  render_posts +
+  '<form action="/" method="POST"><textarea name="text"></textarea> <input type="submit" value="send" id="send"></form>'
+end
+
+post '/' do
+  Pathname.new('posts.txt').open('a') { |f| f.puts(params[:text]) }
+  redirect '/'
+end
+
+def render_posts
+  Pathname.new('posts.txt').read.lines.map do |line|
+    '<p>' + line + '</p>'
+  end.join
+end
